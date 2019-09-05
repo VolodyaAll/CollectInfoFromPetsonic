f588db77cfd2dd753afe032c64841aab4c362a9f
Vladimir Sharay <volodya22all@gmail.com>
2019-09-05 11:30:59 +0300

    Add necessary gems & modules

--- top_gems.rb
+++ top_gems.rb
@@ -1,31 +1,13 @@
-require 'yaml'
-require 'open-uri'
+require 'csv'
+require 'curb'
 require 'nokogiri'
-require 'terminal-table'
 require_relative 'parser'
 
-gem_repo_url = {}
-base_url = 'https://rubygems.org/gems/'
-top_gems = []
+base_url = 'https://www.petsonic.com/snacks-huesos-para-perros/'
+alt_urrl = 'https://www.petsonic.com/hobbit-half/'
 
 # parse command line params
 options = CommandLineParser.parse(ARGV)
-
-# reading list of gems(using command line param -f)
-options[:file] ||= 'gems.yml'
-gems = YAML.load_file(options[:file])
-
-# preparing gem hash according command line parameter -n
-begin
-  gems['gems'].uniq!
-rescue NoMethodError => err
-  puts "File #{options[:file]} is empty. Exception encountered: #{err}"
-  exit 1
-end
-
-gems['gems'].select! { |gem| gem.match?(/#{options[:name]}/) }
-
-raise "There is no gem whith '#{options[:name]}' in name." if gems['gems'].empty?
 
 # getting repo adresses
 gems['gems'].each do |gem|
@@ -66,15 +48,3 @@ gem_repo_url.each do |key, value|
   end
   top_gems << gem_array
 end
-
-# preparing array of gems with params by top algoritm & command line param -t
-top_gems.sort_by! { |arr| arr[1, 6].sum }.reverse!
-
-top_gems = top_gems[0, options[:top].to_i] unless options[:top].nil?
-
-# ouput top gems
-gems_quant = options[:name] ? "with '#{options[:name]}' in name" : ''
-table = Terminal::Table.new(title: "Top #{options[:top]} gems #{gems_quant} from #{options[:file]}",
-                            headings: %w[Gem Used_by Watched Stars Forks Contributors Issues],
-                            rows: top_gems)
-puts table
