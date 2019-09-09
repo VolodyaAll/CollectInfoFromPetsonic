824ccc2539d9230ae7f2cd85b5c0a83454c6a51f
VolodyaAll <volodya22all@gmail.com>
2019-09-09 15:28:57 +0300

    Getting product urls from one page

--- app.rb
+++ app.rb
@@ -15,4 +15,13 @@ html = Nokogiri::HTML(http.body_str)
 
 links_to_categories = html.xpath("//div[@id='subcategories']/ul/li/a").map { |el| el.attr 'href' }
 
-puts links_to_categories
+@product_urls = []
+
+links_to_categories.each do |link_to_category|
+    category_page = Nokogiri::HTML( Curl.get( link_to_category ).body_str )
+    product_links = category_page.xpath("//ul[@id='product_list']/li/div[1]/div/div[1]/a")
+    @product_urls  = product_links.map {|el| el.attr 'href'}.uniq
+    puts @product_urls
+    puts '+++++++++++++++++++++++++++++++++++++++++++++++++++++++++'
+end
+
