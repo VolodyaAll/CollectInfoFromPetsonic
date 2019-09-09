1a6fc520b2553ac3fb54425f1e8b0ce490efe6b3
VolodyaAll <volodya22all@gmail.com>
2019-09-09 14:52:45 +0300

    Getting llinks to categories

--- app.rb
+++ app.rb
@@ -4,50 +4,15 @@ require 'nokogiri'
 require_relative 'parser'
 
 base_url = 'https://www.petsonic.com/snacks-huesos-para-perros/'
-alt_urrl = 'https://www.petsonic.com/hobbit-half/'
+alt_url = 'https://www.petsonic.com/hobbit-half/'
 file = 'results.csv'
 
 # parse command line params
 options = CommandLineParser.parse(ARGV)
 
 http = Curl.get(base_url)
-puts http.body_str
-# # getting repo adresses
-# gems['gems'].each do |gem|
-#   url = base_url + gem
-#   begin
-#     doc = Nokogiri::HTML(URI.open(url))
-#   rescue OpenURI::HTTPError => err
-#     puts "There is no '#{gem}' gem on https://rubygems.org. Exception encountered: #{err}"
-#   else
-#     gem_repo_url[gem] = doc.css('a#code, a#home').last['href']
-#   end
-# end
+html = Nokogiri::HTML(http.body_str)
 
-# # getting array of gems with its params from github
-# gem_repo_url.each do |key, value|
-#   gem_array = []
-#   begin
-#     doc = Nokogiri::HTML(URI.open(value))
-#     doc_for_used_by = Nokogiri::HTML(URI.open(value + '/network/dependents'))
-#   rescue OpenURI::HTTPError => err
-#     puts "Github repositiry '#{gem}' gem not found. Exception encountered: #{err}"
-#   else
-#     gem_array << key
+links_to_categories = html.xpath("//div[@id='subcategories']/ul/li/a").map { |el| el.attr 'href' }
 
-#     # getting used_by
-#     gem_array << doc_for_used_by.css("a[class = 'btn-link selected']").text.gsub!(/[^\d]/, '').to_i
-
-#     # getting watched, stars, forks
-#     doc.css('a.social-count').each do |score|
-#       gem_array << score.text.gsub!(/[^\d]/, '').to_i
-#     end
-
-#     # getting contributors
-#     gem_array << doc.css("span[class ='num text-emphasized']")[3].text.to_i
-
-#     # getting issues
-#     gem_array << doc.at_css('.Counter').text.to_i
-#   end
-#   top_gems << gem_array
-# end
+puts links_to_categories
