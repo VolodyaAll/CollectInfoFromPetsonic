dffd85efbd4b02d0015810a1975bcc214ccfa186
Vladimir Sharay <volodya22all@gmail.com>
2019-09-10 14:56:32 +0300

    Getting product info

--- app.rb
+++ app.rb
@@ -25,7 +25,21 @@ links_to_categories.each do |link_to_category|
   if category_page.xpath("//p[starts-with(@class, 'alert')]").text != 'No hay productos en esta categoría'
     product_links = category_page.xpath("//ul[@id='product_list']/li/div[1]/div/div[1]/a")
     @product_urls  = product_links.map {|el| el.attr 'href'}.uniq
-    puts @product_urls
+
+    @product_urls.each do |product_url|
+      http = Curl.get( product_url )
+      product_page = Nokogiri::HTML( http.body_str )
+      product_name = product_page.xpath("//h1[@class='product_main_name']").text
+
+      product_details = product_page.xpath("//ul[@class='attribute_radio_list']")
+      product_weight = product_details.map { |el| el.xpath("//li/label/span[@class='radio_label']").text }
+      product_price = product_details.map { |el| el.xpath("//li/label/span[@class='price_comb']").text }
+
+      puts product_name
+      puts product_weight
+      puts product_price
+    end
+    
     if Curl.get( link_to_category + "?p=2").body_str.empty?
         puts "empty"
     end
