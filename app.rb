9cb234b3a72e78e48a4fb32374c88710f68215d3
Vladimir Sharay <volodya22all@gmail.com>
2019-09-12 10:38:26 +0300

    Get product weigth links from 1 page

--- app.rb
+++ app.rb
@@ -1,6 +1,7 @@
 require 'csv'
 require 'curb'
 require 'nokogiri'
+require 'mechanize'
 require_relative 'parser'
 
 base_url = 'https://www.petsonic.com/snacks-huesos-para-perros/'
@@ -22,6 +23,7 @@ links_to_categories.each do |link_to_category|
 
   category_page = Nokogiri::HTML( http.body_str )
 
+
   if category_page.xpath("//p[starts-with(@class, 'alert')]").text != 'No hay productos en esta categoría'
     product_links = category_page.xpath("//ul[@id='product_list']/li/div[1]/div/div[1]/a")
     @product_urls  = product_links.map {|el| el.attr 'href'}.uniq
@@ -30,14 +32,24 @@ links_to_categories.each do |link_to_category|
       http = Curl.get( product_url )
       product_page = Nokogiri::HTML( http.body_str )
       product_name = product_page.xpath("//h1[@class='product_main_name']").text
-
-      product_details = product_page.xpath("//ul[@class='attribute_radio_list']")
-      product_weight = product_details.map { |el| el.xpath("//li/label/span[@class='radio_label']").text }
-      product_price = product_details.map { |el| el.xpath("//li/label/span[@class='price_comb']").text }
-
       puts product_name
-      puts product_weight
-      puts product_price
+
+      product_fieldsets = product_page.xpath("//div[@id='attributes']/fieldset[@class='attribute_fieldset']").map 
+      puts product_fieldsets.size
+
+      product_fieldsets.each do |fieldset|
+        product_weights = fieldset.xpath("//div/ul/li/label/span[@class='radio_label']").map { |el| el.text }
+        product_price = fieldset.xpath("//div/ul/li/label/span[@class='price_comb']").map { |el| el.text }
+        product_radio_value = fieldset.xpath("//div/ul/li/input").map { |el| el.attr('value') }
+        product_help_value = fieldset.text.split(' ')[0][0..-2]
+
+        
+        product_weights.each_with_index do |product_weight, index|
+          product_weigth_url = "#{product_url}#/#{product_radio_value[index]}-#{product_help_value}-#{product_weight.gsub(' ', '_')}".downcase
+          puts product_weigth_url
+        end
+
+      end
     end
     
     if Curl.get( link_to_category + "?p=2").body_str.empty?
